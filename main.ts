b4e65c6db81d7a1fb786b2d15fd106303a2c8249
beluscsakzsolt11 <114068039+beluscsakzsolt11@users.noreply.github.com>
2022-11-16 19:04:20 +0100

    Kezdőfájlok a MakeCode projekthez

---
+++ main.ts
@@ -0,0 +1,25 @@
+let szimbolum = 0
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.SmallSquare)
+    basic.pause(30)
+    basic.showIcon(IconNames.Square)
+    basic.pause(30)
+    basic.showIcon(IconNames.Scissors)
+    basic.pause(1)
+    szimbolum = randint(1, 3)
+    if (szimbolum == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # . # .
+            . # # # .
+            . . . . .
+            `)
+    }
+    if (szimbolum == 2) {
+        basic.showIcon(IconNames.Square)
+    }
+    if (szimbolum == 3) {
+        basic.showIcon(IconNames.Scissors)
+    }
+})
